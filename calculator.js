87ee087b7dc780b68a8265a10d6fd6e66469aefc
PrathmeshRai123 <140337431+PrathmeshRai123@users.noreply.github.com>
2023-08-29 20:31:55 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,25 @@
+   use #JAVASCRIPT 
+#it is use for perform calculation using multiplication addition and subtraction
+saved as calculator.js
+..
+.
+    // Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
